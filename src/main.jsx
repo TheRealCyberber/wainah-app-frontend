6336171fba27c048f475f3b6b5793fdfb5d76af8
TheRealCyberber <shafi.albalushi@gmail.com>
2025-08-15 18:50:52 +0300

    CSS issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'leaflet/dist/leaflet.css'
-import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.querySelector('#root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+createRoot(document.querySelector('#root')).render(<App />)
